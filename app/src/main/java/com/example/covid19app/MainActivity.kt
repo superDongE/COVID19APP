e35830b76372c43334b2f3271a37715d64827a82
superDongE <dlagoehd13@gmail.com>
2021-01-29 12:01:26 +0900

    초기 코드 설정

--- app/src/main/java/com/example/covid19app/MainActivity.kt
+++ app/src/main/java/com/example/covid19app/MainActivity.kt
@@ -2,10 +2,11 @@ package com.example.covid19app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.covid19app.config.BaseActivity
+import com.example.covid19app.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity<ActivityMainBinding>(ActivityMainBinding::inflate) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
     }
 }
